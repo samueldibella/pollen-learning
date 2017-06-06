894d6185c60d2f26a844fad9a7a917025135f406
samueldibella <samuel.dibella@gmail.com>
2017-06-05 23:47:53 -0400

    7 Tutorial Intermission: Tags, Tag functions

---
+++ 7_tutorial/article.html.pm
@@ -0,0 +1,9 @@
+#lang pollen
+
+◊(define (strong word) "Arg")
+
+I want to ◊verb{attend} ◊em{◊event{RacketCon} ◊strong{this} year.}
+
+◊div[#:class "author" #:id "primary" #:living "true"]{Prof. Leonard}
+
+◊div[#:class "red" #:style "font-size:150%"]{Important ◊em{News}}
